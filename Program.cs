bd9c42c04cbd5c54389094b5610b11ea98d8a3f4
wblackmon <wayne.blackmon@outlook.com>
2024-12-02 15:09:04 -0600

    Initial checkin

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Objective:
+////Create a class which will parse through the input
+
+//Requirements:
+//Create three different outputs
+////one for a string of unique words delimited by a parameterized character
+////one for a List of only words which have duplicates
+////one for an DTO Array of each word and populate the properties
+
+//Additional Notes:
+////Do not modify the code contained in the Program class
+
+Console.WriteLine("Welcome to the Word Parser Coding Challenge");
+var wordParser = new WordParser();
+var words = GetWords();
+
+var distinctWordsDelimitedString = wordParser.ReturnUniqueWordsDelimited(words, delimiter: ",");
+Console.WriteLine("ReturnUniqueWordsDelimited returned a count of " + distinctWordsDelimitedString.Split(',').Length);
+
+var duplicateWordsList = wordParser.ReturnWordsWithDuplicatesList(words);
+Console.WriteLine("ReturnWordsWithDuplicatesList returned a count of " + duplicateWordsList.Count);
+
+var duplicatesDTOArray = wordParser.ReturnDuplicatesDTO(words);
+Console.WriteLine("ReturnDuplicatesDTO returned a count of " + duplicatesDTOArray.Length);
+
+Console.WriteLine("This concludes the Word Parser Coding Challenge!");
+Console.WriteLine("Thank you for participating!");
+
+/// <summary>
+///  Only use the first column, ignore the second column
+/// </summary>
+/// <returns></returns>
+static Dictionary<string, string> GetWords()
+{
+    var words = new Dictionary<string, string>();
+    words.Add("policeman", "bad_data");
+    words.Add("calculation", " ");
+    words.Add("thread", "bad_data");
+    words.Add("despair", "bad_data");
+    words.Add("reflection", "bad_data");
+    words.Add("penetrate", "bad_data");
+    words.Add("conference", "bad_data");
+    words.Add("worry", "bad_data");
+    words.Add("divide", "bad_data");
+    words.Add("organ", "bad_data");
+    words.Add("limited", "bad_data");
+    words.Add("smile", "bad_data");
+    words.Add("strain", "bad_data");
+    words.Add("expect", "bad_data");
+    words.Add("alcohol", "bad_data");
+    words.Add("moment", "");
+    return words;
+}
---
+++ WordParser.cs
@@ -0,0 +1,82 @@
+﻿
+public class WordParser
+{
+	public string ReturnUniqueWordsDelimited(Dictionary<string, string> words, string delimiter)
+	{
+		var uniqueWords = new Dictionary<string, int>() { };
+
+		foreach (var word in words)
+		{
+			if (uniqueWords.ContainsKey(word.Key))
+			{
+				uniqueWords[word.Key] += 1;
+			}
+			else
+			{
+				uniqueWords.Add(word.Key, 1);
+			}
+		}
+
+		var uniqueWordsString = string.Join(delimiter, uniqueWords.Where(u => u.Value == 1).Select(kvp => kvp.Key));
+		Console.WriteLine($"Unique words: {uniqueWordsString}");
+
+		return uniqueWordsString;
+	}
+
+	public List<string> ReturnWordsWithDuplicatesList(Dictionary<string, string> words)
+	{
+		Dictionary<string, int> duplicateWords = DuplicateWords(words);
+
+		var duplicateWordsList = duplicateWords.Select(kvp => kvp.Key).ToList();
+
+		return duplicateWordsList;
+	}
+
+	private static Dictionary<string, int> DuplicateWords(Dictionary<string, string> words)
+	{
+		var duplicateWords = new Dictionary<string, int>();
+
+		foreach (var word in words)
+		{
+			if (duplicateWords.ContainsKey(word.Key))
+			{
+				duplicateWords[word.Key] += 1;
+			}
+			else
+			{
+				duplicateWords.Add(word.Key, 1);
+			}
+		}
+
+		// Use ToDictionary to create a dictionary from the filtered results
+		duplicateWords = duplicateWords.Where(kvp => kvp.Value > 1)
+									   .ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+		return duplicateWords;
+	}
+	
+	public DuplicatesDTO[] ReturnDuplicatesDTO(Dictionary<string, string> words)
+	{
+		var duplicateWords = DuplicateWords(words);
+
+		List<DuplicatesDTO> dupesDto = new List<DuplicatesDTO>() { };
+
+		foreach (var dupe in duplicateWords)
+		{
+			var tempDTO = new DuplicatesDTO
+			{
+				Word = dupe.Key,
+				Count = dupe.Value
+			};
+
+			dupesDto.Add(tempDTO);
+		}
+		return dupesDto.ToArray();
+	}
+}
+
+public class DuplicatesDTO
+{
+	public string Word { get; set; }
+	public int? Count { get; set; }
+}
